c7d5b82b4dc2cc17d686d0153adf7c31efd707db
efondere <97473864+efondere@users.noreply.github.com>
2023-09-23 22:45:25 -0400

    terminer l'exercice du chapitre 5.1

--- exercice.py
+++ exercice.py
@@ -6,29 +6,87 @@ from typing import List
 
 
 def convert_to_absolute(number: float) -> float:
-    return 0
+    if number < 0:
+        number = -number
+    return number
 
 
 def use_prefixes() -> List[str]:
-    prefixes, suffixe = 'JKLMNOPQ', 'ack'
+    prefixes, suffix = 'JKLMNOPQ', 'ack'
+    name_list = []
+    for prefix in prefixes:
+        name_list.append(prefix + suffix)
 
-    return [""]
+    return name_list
 
 
 def prime_integer_summation() -> int:
-    return 0
+    # somme des 100 premiers nombres premiers
+    prime_numbers = []
+    number = 2
+
+    while len(prime_numbers) < 100:
+        is_divisable = False
+        for prime_number in prime_numbers:
+            if number % prime_number == 0:
+                is_divisable = True
+                break
+
+        if not is_divisable:
+            prime_numbers.append(number)
+
+        number += 1
+        
+    sum = 0
+    for prime_number in prime_numbers:
+        sum += prime_number
+    return sum
 
 
 def factorial(number: int) -> int:
-    return 0
+    result = 1
+    while number > 0:
+        result *= number
+        number -= 1
+
+    return result
 
 
 def use_continue() -> None:
-    pass
+    for i in range(1, 11):
+        if i == 5:
+            continue
+        
+        print(i)
 
 
 def verify_ages(groups: List[List[int]]) -> List[bool]:
-    return []
+    def is_group_acceptable(group):
+        if not 4 <= len(group) <= 10:
+            return False
+
+        has_over_70 = False
+        has_exactly_50 = False
+        has_minor = False
+        for member in group:
+            if member == 25:
+                return True
+            if member < 18:
+                has_minor = True
+            if member == 50:
+                has_exactly_50 = True
+            if member > 70:
+                has_over_70 = True
+
+        if has_minor:
+            return False
+
+        if has_over_70 and has_exactly_50:
+            return False
+
+        return True
+
+    return [is_group_acceptable(g) for g in groups]
 
 
 def main() -> None:
